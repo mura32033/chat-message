109a3b60a59dc962e58c9610b142789c8047a757
muranuun <mura3-2033@hotmail.co.jp>
2024-12-19 22:44:21 +0900

    UIを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import React, { FormEvent, useState } from "react";
 import { Card } from "@aws-amplify/ui-react";
 import { generateRecipe } from "./actions";
 
-
 export default function Home() {
   const [result, setResult] = useState<string>("");
   const [loading, setloading] = useState(false);
@@ -28,11 +27,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center  p-24  m-auto ">
       <div className=" pb-10 mx-auto text-center flex flex-col items-start -center max-w-3xl">
         <h1 className=" text-4xl  font-bold  text-gray-900 sm:text-6xl ">
-          Meet Your Personal <span className=" text-blue-600"> Recipe AI </span>
+          文章を考える時間を
+          <br /> <span className=" text-blue-600">AI</span> に任せる
           <p className=" mt-10 font-medium   text-lg  max-w-prose text-gray-900 ">
-            Simply type few ingredients using the format ingredient1,
-            ingredient12,..etc and Recipe AI will generate an all-new recipe on
-            demand...
+            言いたいことを箇条書きして、送るツールを選んで、どんな温度感の文章を生成するかを選ぶだけで、文章を生成することができます。
           </p>
         </h1>
       </div>
@@ -42,28 +40,48 @@ export default function Home() {
           onSubmit={onSubmit}
           className=" p-4 flex flex-col items-center gap-4  max-w-full mx-auto"
         >
-          <input
-            type="text"
+          <select
+            name="app_type"
+            id="app_type"
+            className="border border-black  text-gray-900 p-4 rounded-lg max-w-full w-full text-xl "
+          >
+            <option value="Slack">Slack</option>
+            <option value="Teams">Teams</option>
+            <option value="Email">メール</option>
+          </select>
+          で
+          <select
+            name="tempareture"
+            id="tempareture"
+            className="border border-black  text-gray-900 p-4 rounded-lg max-w-full w-full text-xl "
+          >
+            <option value="neutral">普通</option>
+            <option value="request">依頼</option>
+            <option value="thank">感謝</option>
+            <option value="apologize">謝罪</option>
+          </select>
+          したいことを箇条書きすると、
+          <textarea
             id="ingredients"
             name="ingredients"
             required
-            placeholder="Ingredient1, Ingredient2, Ingredient3,..etc"
+            placeholder="言いたいことを箇条書きする"
             className="border border-black  text-gray-900 p-4 rounded-lg max-w-full w-full text-xl "
+            rows={5}
           />
           <button
             type="submit"
             className="  text-white p-2 rounded-lg bg-blue-500   w-1/2 text-xl  "
           >
-            Generate
+            よろしく
           </button>
         </form>
       </section>
       {loading ? (
         <div className="flex flex-col items-center gap-4 w-1/2  mx-auto ">
           <h2 className="m-10 font-medium   text-xl   max-w-prose text-blue-600 ">
-            Wait for it...
+            しばし待たれよ...
           </h2>
-
         </div>
       ) : (
         <div>
